fcb032e5a92326096fd862adcb8dcd245fd4f785
Dionnys <dionnys.shakaw@hotmail.com>
2017-08-28 21:49:19 -0300

    Projeto Eventos LPC

---
+++ eventos/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from eventos.models import Pessoa, Endereco, PessoaFisica, Evento, Inscricao
+admin.site.register(Pessoa)
+admin.site.register(Endereco)
+admin.site.register(PessoaFisica)
+admin.site.register(Evento)
+admin.site.register(Inscricao)
